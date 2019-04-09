e170cdd4f86523cce440c199cf205c73c5e01ecb
thoan741 <than5466@student.su.se>
2019-04-09 21:33:23 +0200

    Simple calculator draft

---
+++ Laboration_3/Calculator/src/Calculator.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static class stack {
+
+        double[] myArray = new double[10000];
+        double value;
+        int pointer = 0;
+
+
+        public void push(double var) {
+            this.myArray[pointer] = var;
+            this.pointer++;
+        }
+
+        public void pop() {
+            this.pointer--;
+            this.value = this.myArray[this.pointer];
+            this.myArray[this.pointer] = 0;
+        }
+
+        public void is_empty() {
+            if (this.pointer != 0) {
+                System.out.println("True");
+            } else {
+                System.out.println("False");
+            }
+        }
+
+        public void calc() {
+            this.pop();
+            double a = this.value;
+            this.pop();
+            double b = this.value;
+            double d = a+b;
+            this.push(d);
+            System.out.println(d);
+        }
+    }
+
+    public static void main(String[] args) {
+        stack A = new stack();
+        while (true) {
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Enter command");
+            String command = myObj.nextLine();
+            String[] splitted = command.split(" ");
+
+
+            for (int i = 0; i < splitted.length; i++) {
+                if (splitted[i].equals("+")) {
+                    A.calc();
+
+                } else {
+
+                    double d = Double.parseDouble(splitted[i]);
+                    A.push(d);
+                }
+            }
+        }
+    }
+}
