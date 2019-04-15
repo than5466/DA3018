ff171f1fa6695cf41f7366a3a71eadf446b4a5e1
thoan741 <than5466@student.su.se>
2019-04-15 14:52:08 +0200

    Typo fix

--- Calculator/src/calculator/Stack.java
+++ Calculator/src/calculator/Stack.java
@@ -2,44 +2,37 @@ package calculator;
 
 public class Stack {
 	/**
-     * Create a new class named Stack.
-     * When initializing an instance of this class,
-     * all the variables and methods below are
-     * available for that object.
-     */
+	 * Create a new class named Stack. When initializing an instance of this class,
+	 * all the variables and methods below are available for that object.
+	 */
 
-    double[] stackArray = new double[10000];
-    int length = stackArray.length;
-    int pointer = 0;
+	double[] stackArray = new double[10000];
+	int length = stackArray.length;
+	int pointer = 0;
 
+	public void push(double var) {
+		/**
+		 * This is a method for pushing a variable of type double onto the stack. If the
+		 * stack is full, an error flag is raised.
+		 */
+		stackArray[pointer] = var;
+		pointer++;
+	}
 
-    public void push(double var) {
-        /**
-         * This is a method for pushing a variable
-         * of type double onto the stack.
-         * If the stack is full, an error flag
-         * is raised.
-         */
-            stackArray[pointer] = var;
-            pointer++;
-    }
+	public double pop() {
 
-    public double pop() {
+		/**
+		 * A method for taking out the top object from the stack.
+		 */
 
-        /**
-         * A method for taking out the top
-         * object from the stack.
-         */
-            pointer--;
-            return stackArray[pointer];
-    }
+		pointer--;
+		return stackArray[pointer];
+	}
 
-    public boolean is_empty() {
-        /**
-         * Simply returns true or false,
-         * depending on whether
-         * the stack is empty or not.
-         */
-        return pointer == 0;
-    }
+	public boolean is_empty() {
+		/**
+		 * Simply returns true or false, depending on whether the stack is empty or not.
+		 */
+		return pointer == 0;
+	}
 }
