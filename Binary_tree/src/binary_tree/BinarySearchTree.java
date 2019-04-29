34dd0d42e3392c5e6a803319441296b3e13662a0
thoan741 <than5466@student.su.se>
2019-04-29 15:47:51 +0200

    More comments

--- Binary_tree/src/binary_tree/BinarySearchTree.java
+++ Binary_tree/src/binary_tree/BinarySearchTree.java
@@ -11,7 +11,6 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 	BSTNode root = null;
 
 	public BinarySearchTree() {
-		// Empty constructor?
 	}
 
 	/**
@@ -29,7 +28,8 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 	}
 
 	/**
-	 * Insert 'node' into the tree pointed at by 'root'.
+	 * Insert 'node' into the tree pointed at by 'root'. If a node with the same code already exist,
+	 * overwrite it with the new information.
 	 * 
 	 * @returns The node that should be the root of this subtree.
 	 * @param root
@@ -59,12 +59,22 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 
 	/**
 	 * size: Count the number of nodes in the search tree
+	 * @return
+	 * calls the size function defined under this one with the parameter root.
 	 */
 
 	public int size() {
 
 		return size(root);
 	}
+	
+	/**
+	 * 
+	 * @param node
+	 * @return
+	 * returns 0 if the current node is null, otherwise returns 
+	 * 1 + recursively the remove function on the right and left node node, respectively.
+	 */
 
 	public int size(BSTNode node) {
 		if (node == null) {
@@ -83,6 +93,18 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 	public BSTNode find(String courseCode) {
 		return find(courseCode, root);
 	}
+	
+	/**
+	 * A comparison is made between the current node and the one searched for.
+	 * If the current node is of higher value than the
+	 * one searched for, recursively call this function but with the left child of the current node, otherwise
+	 * recursively call the function with the right child.
+	 * @param courseCode
+	 * @param node
+	 * @return
+	 * 
+	 * If they're the same, return the current node. If it doesn't exist, return null.
+	 */
 
 	public BSTNode find(String courseCode, BSTNode node) {
 		if (node == null) {
@@ -100,12 +122,32 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 		}
 
 	}
+	
+	/**
+	 * remove: removes the current node searched for.
+	 * @param courseCode
+	 */
 
 	public void remove(String courseCode) {
 
 		root = remove(courseCode, root);
 
 	}
+	
+	/**
+	 * 
+	 * compares the course code of the node that should be deleted with the current.
+	 * If the current node has a smaller value, recursively set the current nodes right child
+	 * to the remove function applied to the right child, and do nothing with the left, and 
+	 * respectively call the remove on the left child if it has a higher value. 
+	 * @param courseCode
+	 * @param node
+	 * @return
+	 * return null if the node doesn't exist in the tree. Otherwise, if the searched for node doesn't
+	 * have a right child, return the left and if no left child, return right.
+	 * If however the node has a right, as well as a left child, the left node is inserted into
+	 * the right and then returned.
+	 */
 
 	public BSTNode remove(String courseCode, BSTNode node) {
 
@@ -196,6 +238,7 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 		}
 
 	}
+
 	/**
 	 * Creating a new instance of the iterator class defined below
 	 * 
@@ -205,7 +248,7 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 	public Iterator<BSTNode> iterator() {
 		return new BSTIterator(root);
 	}
-	
+
 	/**
 	 * Iterator class that iterates through a BST in order.
 	 * 
@@ -219,7 +262,7 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 		private boolean go;
 		private String savedCode;
 		private int compare;
-		
+
 		/**
 		 * 
 		 * @param node - an object of the BSTNode class.
@@ -233,19 +276,21 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 			this.savedCode = null;
 
 		}
+
 		/**
 		 * 
 		 * @param node - an object of the BSTNode class
-		 * @return the course code from the node which has the highest valued course code.
+		 * @return the course code from the node which has the highest valued course
+		 *         code.
 		 */
 
-		public String FinalCode (BSTNode node) {
+		public String FinalCode(BSTNode node) {
 			while (node.getRightChild() != null) {
 				node = node.getRightChild();
 			}
 			return node.getCourseCode();
 		}
-		
+
 		/**
 		 * returns false if the course code of the node which next() returned previously
 		 * is the same as the highest valued one, true otherwise.
@@ -254,13 +299,14 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 		public boolean hasNext() {
 			return this.previousCode != this.finalCode;
 		}
-		
+
 		/**
-		 * returns the course code which the method nextInOrder decides.
-		 * also updates the previousCode parameter to make sure that the iterator doesn't get stuck in
-		 * an endless loop.
+		 * returns the course code which the method nextInOrder decides. also updates
+		 * the previousCode parameter to make sure that the iterator doesn't get stuck
+		 * in an endless loop.
 		 * 
-		 * Also sets the current node to the original root, before calling the nextInOrder method.
+		 * Also sets the current node to the original root, before calling the
+		 * nextInOrder method.
 		 */
 
 		public BSTNode next() {
@@ -278,20 +324,21 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 			}
 
 		}
-		
+
 		/**
-		 * Decides which node is the next, with the help of two functions goLeft and goRight.
-		 * If the current node has a higher value than the one returned in the last iteration, 
-		 * the goLeft function is called until the current node isn't bigger than the previous.
-		 * Then the right function is called. This is done until the next node for certainty isn't
-		 * any deeper in the tree.
-		 * The while loop is cancelled when the current child doesn't have a right child,
-		 * but the current node has a smaller or equal value than the last one, or vice versa
+		 * Decides which node is the next, with the help of two functions goLeft and
+		 * goRight. If the current node has a higher value than the one returned in the
+		 * last iteration, the goLeft function is called until the current node isn't
+		 * bigger than the previous. Then the right function is called. This is done
+		 * until the next node for certainty isn't any deeper in the tree. The while
+		 * loop is cancelled when the current child doesn't have a right child, but the
+		 * current node has a smaller or equal value than the last one, or vice versa
 		 * for the left child.
-		 * @return
-		 * When the next course code for certain isn't any deeper in the tree, the find function is used to 
-		 * locate the node which value is greater, but with the smallest margin, than the previous code,
-		 * and then returns it.
+		 * 
+		 * @return When the next course code for certain isn't any deeper in the tree,
+		 *         the find function is used to locate the node which value is greater,
+		 *         but with the smallest margin, than the previous code, and then
+		 *         returns it.
 		 * 
 		 */
 
@@ -320,19 +367,20 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 						this.go = false;
 					}
 
-					
 				}
 			}
 
 			return find(savedCode, this.root);
 		}
-		
+
 		/**
-		 * While the current node is of a higher value than the previous, set current to the left child.
-		 * if a node with a higher value is found, set the savedCode to the course name of the code.
-		 * @return
-		 * returns current node whenever the node doesn't have a left child or the current node has a lower
-		 * or equal value than the one found in the previous iteration.
+		 * While the current node is of a higher value than the previous, set current to
+		 * the left child. if a node with a higher value is found, set the savedCode to
+		 * the course name of the code.
+		 * 
+		 * @return returns current node whenever the node doesn't have a left child or
+		 *         the current node has a lower or equal value than the one found in the
+		 *         previous iteration.
 		 */
 
 		public BSTNode goLeft() {
@@ -347,13 +395,15 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 			return this.current;
 
 		}
+
 		/**
-		 * While the current node is of a lower or equal value than the one from the previous iteration,
-		 * set current to the right child.
-		 * if a node with a higher value is found, set the savedCode to the course name of the code.
-		 * @return
-		 * returns current node whenever th node doesn't have a right child or the current node has a higher
-		 * value than the one found in the previous iteration.
+		 * While the current node is of a lower or equal value than the one from the
+		 * previous iteration, set current to the right child. if a node with a higher
+		 * value is found, set the savedCode to the course name of the code.
+		 * 
+		 * @return returns current node whenever th node doesn't have a right child or
+		 *         the current node has a higher value than the one found in the
+		 *         previous iteration.
 		 */
 
 		public BSTNode goRight() {
@@ -367,6 +417,7 @@ public class BinarySearchTree implements Iterable<BinarySearchTree.BSTNode> {
 			return this.current;
 
 		}
+
 	}
 
 }
